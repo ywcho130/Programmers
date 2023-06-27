76045e58824fd18834a70400bf85d4c081c43007
yongwoo <yongwoowh@naver.com>
2023-06-27 20:38:30 +0900

    프로그래머스 JAVA LV_1 (나누어 떨어지는 숫자 배열) 학습

---
+++ Programmers/src/Lv_1/DivisibleNumericArray.java
@@ -0,0 +1,33 @@
+package Lv_1;
+
+
+public class DivisibleNumericArray {
+	public int[] solution(int[] arr, int divisor) {
+
+		int count = 0;
+		int number = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % divisor == 0) {
+				count++;
+			}
+		}
+
+		if (count == 0) {
+			int[] answer = { -1 };
+			return answer;
+		}
+
+		int[] answer = new int[count];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] % divisor == 0) {
+				answer[number] = arr[i];
+				number++;
+			}
+		}
+
+		return answer;
+
+	}
+}
